69af924fc28c063df86915b6a97616a169299b7f
YUUKIToriyama <github@toriyama.dev>
2021-08-06 01:14:33 +0900

    使用方法をまとめたページを作成

--- src/main.js
+++ src/main.js
@@ -7,9 +7,8 @@ const config = {
 	port: 3000
 }
 
-app.get("/", (request, response) => {
-	response.send("Hello, world!");
-});
+// http://localhost:3000
+app.use("/", express.static("public"));
 // http://localhost:3000/latlng2address?lng=35.04486&lat=135.73016
 app.get("/reverseGeoCoding", async (request, response) => {
 	const longitude = parseFloat(request.query.lng);
