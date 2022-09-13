8525ef047cb9a40a6ef04131721e25e0128e9ca7
YUUKIToriyama <github@toriyama.dev>
2022-09-14 02:31:27 +0900

    typescript + fastify の構成で書き換えた

---
+++ src/main.ts
@@ -0,0 +1,68 @@
+import fastify from 'fastify';
+import { fastifyStatic } from "@fastify/static";
+import cors from "@fastify/cors";
+import * as path from "path"
+import { openReverseGeocoder } from '@geolonia/open-reverse-geocoder';
+import { normalize } from '@geolonia/normalize-japanese-addresses';
+
+const app = fastify({
+	logger: true
+});
+
+app.register(fastifyStatic, {
+	root: path.join(process.cwd(), "public")
+});
+
+app.register(cors);
+
+interface ReverseGeoCodingQueryParameter {
+	lat: number
+	lng: number
+}
+app.get<{ Querystring: ReverseGeoCodingQueryParameter }>("/reverseGeoCoding", async (request, reply) => {
+	const longitude = request.query.lat;
+	const latitude = request.query.lng;
+	try {
+		const result = await openReverseGeocoder([longitude, latitude]);
+		reply.send(result);
+	} catch (error) {
+		reply.send({
+			"Error": {
+				"message": "Invalid parameter"
+			}
+		});
+	}
+});
+
+interface NormalizeAddressQueryParameter {
+	address: string
+	level?: number
+}
+app.get<{ Querystring: NormalizeAddressQueryParameter }>("/normalizeAddress", async (request, reply) => {
+	const address = request.query.address;
+	const level = request.query.level || 3;
+	try {
+		const result = await normalize(address, {
+			level: level
+		});
+		reply.send(result);
+	} catch (error) {
+		reply.send({
+			"Error": {
+				"message": error
+			}
+		});
+	}
+});
+
+(async () => {
+	const port = process.env.PORT !== undefined ? parseInt(process.env.PORT) : 3000;
+	try {
+		await app.listen({
+			port: port
+		});
+	} catch (error) {
+		app.log.error(error);
+		process.exit(1);
+	}
+})();
